9e499f93eed9d12145b970e47e949b9154ef7892
Pedro Guilherme Sales <pedroguilhermesalesalmeida@gmail.com>
2025-05-30 12:17:10 -0300

    Mesclador de Arquivos

--- merger.py
+++ merger.py
@@ -1,6 +1,8 @@
 import PyPDF2 as py
 import os
 
+#Guide: to use the Archive Merger you just need to put the archives that you want to merge in the directory 'archives' and run the code bellow.
+
 merger = py.PdfMerger()
 lista_arquivos = os.listdir("arquivos")
 lista_arquivos.sort()
